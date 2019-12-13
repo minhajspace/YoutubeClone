499b0b5885b0dd1f2b42e7713a1e73f14ce9fffb
minhajspace <minhaj.space@gmail.com>
2019-12-13 17:47:52 +0530

    getting video from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import SearchBox from "./SearchBox";
 import Youtube from "../apis/Youtube";
+
 class App extends React.Component {
-  onSearchSubmit = inputText => {
-    Youtube.get("/search", {
+  state = { video: [] };
+
+  onSearchSubmit = async inputText => {
+    const response = await Youtube.get("/search", {
       params: {
         q: inputText
       }
     });
+    console.log(response);
+    this.setState({ video: response.data.items });
   };
-
   render() {
     return (
       <div className="ui container">
-        <SearchBox onSearchSubmit={this.onSearchSubmit} />
+        <SearchBox onSearchSubmit={this.onSearchSubmit} />i have{" "}
+        {this.state.video.length} video
       </div>
     );
   }
