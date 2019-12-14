cbb45604b502d02e22163c40a725016d63573024
minhajspace <minhaj.space@gmail.com>
2019-12-14 15:08:07 +0530

    adding iframe

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -4,11 +4,15 @@ const VideoDetails = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
-        <p4 className="">{video.snippet.description}</p4>
+        <p className="">{video.snippet.description}</p>
       </div>
     </div>
   );
