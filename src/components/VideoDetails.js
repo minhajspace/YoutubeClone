0aaf036f20072bb98261f5e3d308f1fad202b84a
minhajspace <minhaj.space@gmail.com>
2019-12-14 14:53:13 +0530

    Styling  VideoDetails

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -4,6 +4,13 @@ const VideoDetails = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
-  return <div>{video.snippet.title}</div>;
+  return (
+    <div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p4 className="">{video.snippet.description}</p4>
+      </div>
+    </div>
+  );
 };
 export default VideoDetails;
